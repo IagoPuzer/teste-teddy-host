8833d8420350ce6031e015b9678de108b90fe365
iagop <iagopuzer06@hotmail.com>
2025-03-03 16:25:47 -0300

    add initial layout and home components with basic structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,28 @@
 import { loadRemoteModule } from '@angular-architects/native-federation';
 import { Routes } from '@angular/router';
+import { MainLayoutComponent } from './layouts/main-layout/main-layout.component';
+import { DashboardLayoutComponent } from './layouts/dashboard-layout/dashboard-layout.component';
+import { HomeComponent } from './pages/home/home.component';
 
 export const routes: Routes = [
   {
+    path: '',
+    component: MainLayoutComponent,
+    children: [
+      { path: '', component: HomeComponent },
+      // outras rotas para o layout principal
+    ],
+  },
+  {
     path: 'microfrontend',
-    loadComponent: () =>
-      loadRemoteModule('mfe1', './Component').then((m) => m.HomeComponent),
+    component: DashboardLayoutComponent,
+    children: [
+      {
+        path: '',
+        loadComponent: () =>
+          loadRemoteModule('mfe1', './Component').then((m) => m.HomeComponent),
+      },
+      // outras rotas para o layout de dashboard
+    ],
   },
 ];
