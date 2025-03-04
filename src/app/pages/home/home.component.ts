1f891e72f7f4e404c31ffb976966e9bb8dbaee76
iagop <iagopuzer06@hotmail.com>
2025-03-04 16:48:03 -0300

    refactor main layout and update home component to include login form

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { LoginFormComponent } from '../../components/login-form/login-form.component';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  standalone: true,
+  imports: [LoginFormComponent],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent {
+  constructor(private router: Router) {}
 
+  handleLogin(name: string) {
+    console.log(name);
+    this.router.navigate(['/clients']);
+  }
 }
